2edd2306a7eb51dca33b4174148ae2e5b4e9b03f
tmalsheuskaya <t.malsheuskaya@gmail.com>
2026-04-08 13:58:05 +0200

    strangers task fixed

--- src/main/java/HelloStrangers.java
+++ src/main/java/HelloStrangers.java
@@ -1,19 +1,25 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class HelloStrangers {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int count = scanner.nextInt();
-        scanner.nextLine();
+        int count = Integer.parseInt(scanner.nextLine().trim());
 
         if (count < 0 ) {
             System.out.println("Seriously? Why so negative?");
         } else if (count == 0) {
             System.out.println("Oh, it looks like there is no one here");
         } else {
+            List<String> names = new ArrayList<>();
+
             for (int i = 0; i < count; i++) {
-                String name = scanner.nextLine();
+                names.add(scanner.nextLine().trim());
+            }
+
+            for (String name : names) {
                 System.out.println("Hello, " + name);
             }
         }
